fa64e0731e77a692bc5b52d79f777661a3bca759
neeraj001122 <admobid0011>
2022-09-04 09:28:20 +0545

    new file

---
+++ task3.js
@@ -0,0 +1,5 @@
+var headertitle = document.getElementById('header-title');
+headertitle.style.borderBottom = 'solid 3px #000'
+var add = document.getElementById('Add-items');
+ add.style.color='green'
+ add.style.fontWeight = 'bold'
